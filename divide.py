1289bb92e1da03fd486891f20244ff97b69c4092
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2021-08-23 12:41:59 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,37 @@
+# user=int(input("enter the number"))
+# if user % 2==0:
+#     print("number divide hai")
+# else:
+#     # print("number divide nahi hai")
+
+# name=input("enter the name")
+# n=""
+# i=1
+# while i<=len(name):
+#     n+=name[-i]
+#     i=i+1
+# print(n)
+
+
+# num=[1,2,3,4,5,6,7,8]
+# name=int(input("enter the number"))
+# n=[]
+# i=1
+# while i<=len(num):
+#     k=num[-i]
+#     n.append(k)
+#     i=i+1
+# print(n)
+
+
+num=[1,2,3,4,5,6,7,8]
+name=int(input("enter the number"))
+n=[]
+i=1
+while i<=len(num):
+    k=num[-i]
+    n.append(k)
+    i=i+1
+print(n)
+
+
---
+++ true or false no2.py
@@ -0,0 +1,3 @@
+if False:
+    print("true")
+print("false")
